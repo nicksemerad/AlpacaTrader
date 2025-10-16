101fd9509731e98d99bd7d3f6fadff672131c181
nicksemerad <nicksemerad@gmail.com>
2025-10-15 20:35:38 -0600

    initial commit

---
+++ AlpacaTrader/Component/Bar.cs
@@ -0,0 +1,38 @@
+﻿namespace Component;
+
+using System.Text.Json.Serialization;
+
+/// <summary>
+///   Bar object, for use with the Alpaca API that returns JSON objects representing bars.
+/// </summary>
+public class Bar
+{
+    [JsonPropertyName("t")]
+    public DateTime Timestamp { get; set; }
+    
+    [JsonPropertyName("o")]
+    public decimal Open { get; set; }
+    
+    [JsonPropertyName("h")]
+    public decimal High { get; set; }
+    
+    [JsonPropertyName("l")]
+    public decimal Low { get; set; }
+    
+    [JsonPropertyName("c")]
+    public decimal Close { get; set; }
+    
+    [JsonPropertyName("v")]
+    public long Volume { get; set; }
+    
+    [JsonPropertyName("n")]
+    public int TradeCount { get; set; }
+    
+    [JsonPropertyName("vw")]
+    public decimal VolumeWeightedAverage { get; set; }
+
+    public Bar()
+    {
+        
+    }
+}
---
+++ AlpacaTrader/Request/Request.cs
@@ -0,0 +1,56 @@
+﻿namespace Request;
+
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+
+/// <summary>
+///   This class handles making requests to the alpaca API endpoints
+/// </summary>
+public class Request
+{
+    private readonly RestClient _client;
+    private readonly RestRequest _request;
+    
+    public Request(string url)
+    {
+        RestClientOptions opts = new RestClientOptions(url);
+        _client = new RestClient(opts);
+        _request = new RestRequest();
+        AddHeaders(_request);
+    }
+
+    private static void AddHeaders(RestRequest request)
+    {
+        // get the user secrets configuration
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Request>()
+            .Build();
+
+        // get the api key and secret key from the configuration
+        string? apiKey = configuration["API"];
+        string? secretKey = configuration["SECRET"];
+
+        // make sure the keys aren't null
+        if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(secretKey))
+            throw new ArgumentException("Alpaca api key or private key not found.");
+
+        // add the headers
+        request.AddHeader("accept", "application/json");
+        request.AddHeader("APCA-API-KEY-ID", apiKey);
+        request.AddHeader("APCA-API-SECRET-KEY", secretKey);
+    }
+
+    public async Task<string> GetAsync()
+    {
+        RestResponse response = await _client.GetAsync(_request);
+        return response.Content;
+    }
+
+    public static async Task Main(string[] args)
+    {
+        string end = Endpoint.LatestBars(["AAPL", "TSLA"]);
+        Request request = new Request(end);
+        string content = await request.GetAsync();
+        Console.WriteLine(content);
+    }
+}
