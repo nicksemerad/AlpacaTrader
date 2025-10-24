c6b02f20def1f9d2f2a743e4baa9875f16009ee6
nicksemerad <nicksemerad@gmail.com>
2025-10-23 20:11:00 -0600

    Add order Id property to Order

--- AlpacaTrader/Component/Order.cs
+++ AlpacaTrader/Component/Order.cs
@@ -3,27 +3,37 @@ using Common;
 
 namespace Component;
 
+/// <summary>
+///   This class represents an order.
+/// </summary>
 public class Order
 {
+    [JsonPropertyName("order_id")]
+    public string OrderId { get; set; }
+    
+    [JsonPropertyName("symbol")] // required
     public string Symbol { get; set; }
     
     [JsonPropertyName("qty")]
     public int Quantity { get; set; } // num shares to trade
     
+    [JsonPropertyName("filled_qty")]
+    public int FilledQuantity { get; set; }
+    
     [JsonPropertyName("notional")]
     public int Notional { get; set; } // dollar amount to trade (cant do with qty)
     
-    [JsonPropertyName("side")]
+    [JsonPropertyName("side")] // required
     public OrderSide Side { get; set; } // Buy or Sell
     
-    [JsonPropertyName("type")]
+    [JsonPropertyName("type")] // required
     public OrderType OrderType { get; set; } // type or order
     // Equity trading: market, limit, stop, stop_limit, trailing_stop.
     // Options trading: market, limit.
     // Multileg Options trading: market, limit.
     // Crypto trading: market, limit, stop_limit.
     
-    [JsonPropertyName("time_in_force")]
+    [JsonPropertyName("time_in_force")] // required
     public TimeInForce TimeInForce { get; set; } // how long the reade is valid for
     /*
      * day: rest of the day, or queued to start next day
