9827b3f2c85c89cb18ce61c1e1f8c6ca6e64a388
nicksemerad <nicksemerad@gmail.com>
2025-10-28 13:03:53 -0600

    Add a new Indicators project which will handle indicator calculations with Skender stock Indicators package

---
+++ AlpacaTrader/Indicators/Indicators.cs
@@ -0,0 +1,19 @@
+﻿using Component;
+using Skender.Stock.Indicators;
+
+namespace Indicators;
+/// <summary>
+///   This class handles stock price indicators' calculations so they can be used in strategies, visualizations,
+///   backtesting, etc.
+/// </summary>
+public class Indicators
+{
+    public static void Main(string[] args)
+    {
+        Bar b = new Bar();
+        // Quote q = new Quote(b.Timestamp, b.Open, b.High, b.Low, b.Close, b.Volume);
+        List<Bar> quotes = [];
+        IEnumerable<SmaResult> results = quotes.GetSma(20);
+        
+    }
+}
