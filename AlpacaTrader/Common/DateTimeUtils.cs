57e678077e521a0cdb352052725cd45d69441015
nicksemerad <nicksemerad@gmail.com>
2025-10-28 21:00:51 -0600

    Rename DateFormats to DateTimeUtils and add a method to convert EST DateTimes to UTC

--- AlpacaTrader/Common/DateTimeUtils.cs
+++ AlpacaTrader/Common/DateTimeUtils.cs
@@ -1,12 +1,14 @@
 namespace Common;
 
+using System.Globalization;
+
 /// <summary>
 ///   This class contains the date-time string format used by the Alpaca API, and two methods. The first method takes
 ///   in a DateTime object and returns it as a string in the API format, the other takes in a string in the API format
 ///   and returns it as a DateTime object.
 ///   <code>string format = "yyyy-MM-ddTHH:mm:ssZ";</code>
 /// </summary>
-public static class DateFormats
+public static class DateTimeUtils
 {
     /// <summary>
     ///   The format used by Alpaca API URLs and endpoint responses.
@@ -26,5 +28,24 @@ public static class DateFormats
     /// <param name="date">The date string in Alpaca API format: "yyyy-MM-ddTHH:mm:ssZ"(</param>
     /// <returns>The string's date time parsed into a C# DateTime object</returns>
     public static DateTime ToDateTime(string date) =>
-        DateTime.ParseExact(date, ApiDateTimeFormat, System.Globalization.CultureInfo.InvariantCulture);
+        DateTime.ParseExact(date, ApiDateTimeFormat, new CultureInfo("en-US"));
+
+    /// <summary>
+    ///   Converts a DateTime object that is in Eastern Standard Time (EST) to be in UTC instead.
+    /// </summary>
+    /// <param name="dateTime">The DateTime object in EST that needs to be converted</param>
+    /// <returns>A new DateTime object that has the same time as the parameter, but in UTC</returns>
+    public static DateTime ConvertEstToUtc(DateTime dateTime)
+    {
+        try
+        {
+            var estZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            return TimeZoneInfo.ConvertTimeToUtc(dateTime, estZoneInfo);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.GetType()} occurred while converting DateTime in EST to UTC: {ex.Message}");
+            throw;
+        }
+    }
 }
