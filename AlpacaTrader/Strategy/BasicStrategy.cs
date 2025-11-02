05b856d442df7e6895b525810e70ef5d842cf82f
nicksemerad <nicksemerad@gmail.com>
2025-11-01 23:01:16 -0600

    Update StrategyBase.cs to have abstract methods that handle Initializing and Updating indicators. Add empty overrides to the BasicStrategy.cs

--- AlpacaTrader/Strategy/BasicStrategy.cs
+++ AlpacaTrader/Strategy/BasicStrategy.cs
@@ -1,6 +1,9 @@
-﻿using Component;
+﻿namespace Strategy;
 
-namespace Strategy;
+using Component;
+using Skender.Stock.Indicators;
+using Indicators;
+
 
 /// <summary>
 ///   This class is an extremely basic trading strategy, and isn't implemented yet.
@@ -8,12 +11,39 @@ namespace Strategy;
 public class BasicStrategy : StrategyBase
 {
     /// <summary>
+    ///   The bars representing a stock's historical prices.
+    /// </summary>
+    private List<Bar> _bars;
+    private List<SmaResult> _smaResults;
+    
+    public BasicStrategy(List<Bar> bars) : base(bars)
+    {
+        _bars = bars;
+        _smaResults = Indicators.GetSmaSeries(bars, 5);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void InitializeIndicators()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void UpdateIndicators()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
     /// 
     /// </summary>
     /// <param name="bars"></param>
     /// <param name="currentBar"></param>
     /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
     public override int GetSignal(IList<Bar> bars, Bar currentBar)
     {
         throw new NotImplementedException();
--- AlpacaTrader/Strategy/StrategyBase.cs
+++ AlpacaTrader/Strategy/StrategyBase.cs
@@ -3,26 +3,51 @@ namespace Strategy;
 using Component;
 
 /// <summary>
-///   This abstract class contains a number of methods that a strategy may use, specifically calculations for technical
-///   indicators. There are also a number of abstract methods, which are methods that any Strategy class must
-///   implement.
+///   This abstract class contains methods that any derived strategy needs to implement, and will use to calculate
+///   signals based on the bars it is provided. 
 /// </summary>
 public abstract class StrategyBase
 {
-    private IList<Bar> _bars;
-    
+    /// <summary>
+    ///   The bars representing a stock's historical prices.
+    /// </summary>
+    private List<Bar> _bars;
+
+    protected StrategyBase(List<Bar> bars) => _bars = bars;
+
+    /// <summary>
+    ///   Initializes the list of historical bars as well as the technical indicators that are used to calculate
+    ///   signals.
+    /// </summary>
+    /// <param name="historicalBars"></param>
     public void Initialize(List<Bar> historicalBars)
     {
         _bars = historicalBars;
+        InitializeIndicators();
     }
-    
+
+    /// <summary>
+    ///   Adds a new bar to the list of bars, which updates the current price history.
+    /// </summary>
+    /// <param name="newBar"></param>
     public void Update(Bar newBar)
     {
         _bars.Add(newBar);
+        UpdateIndicators();
     }
-    
+
     /// <summary>
-    ///   When there is a new latest bar, use the past bars to determine the strategy's signal (i.e. buy, sell, wait)
+    ///   Initializes the technical indicators that a strategy uses to calculate signals.
+    /// </summary>
+    protected abstract void InitializeIndicators();
+
+    /// <summary>
+    ///   Updates the technical indicators that a strategy uses to calculate signals.
+    /// </summary>
+    protected abstract void UpdateIndicators();
+
+    /// <summary>
+    ///   When there is a new latest bar, use the past bars to determine the strategy's signal (i.e. buy, sell, hold)
     /// </summary>
     /// <param name="bars">The past bars</param>
     /// <param name="latestBar">The latest bar</param>
